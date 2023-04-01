52695e9d2c2627269a57552c01d7a081dc2334da
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2023-04-01 15:44:42 -0700

    js syntax

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.alert("I am learning Javascript!");
 alert("Hello Javascript!");
 
 // document.write shows output on the html page 
-document.write("I am learning Javascript!");
+// document.write("I am learning Javascript!"); 
 
 // to write something inside of a  html tag innerHTML is used and this too shows output on html page
 document.getElementById("root").innerHTML="I love learning Javascript!";
@@ -12,5 +12,9 @@ document.getElementById("idH3").innerHTML="Hello World!, I am H3";
 
 // to write something inside the console of a browser console.log is used 
 console.log("Hello Javascript!");
-console.log("I am line one.");
-console.log("I am line two.");
+console.log("I am line 'one'.");
+console.log('I am line "two".');
+document.write(67.98);
+
+/* this is multiple 
+line commnet. */
