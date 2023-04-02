03c0817a8120b331ece3b8f932e63a6ea842767f
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2023-04-01 17:05:30 -0700

    variable, constant and keywords

---
+++ js/variable.js
@@ -0,0 +1,34 @@
+// var a = 20;
+// var b = 30;
+// var c = a+b;
+// console.log(c);
+// let b;
+// a = 98;
+// b = "Hello Javascript!";
+// console.log(a);
+// console.log(b);
+// a = 96.34;
+// console.log(a);
+// a = "Tahmid Jawad An-Noor";
+// console.log(a);
+// var b = a;
+
+// console.log(a);
+// console.log(b);
+// a = 30;
+// console.log(a);
+// console.log(b);
+
+// js variables are case sensitive 
+var MyName, myName;
+myName = "Tahmid";
+MyName = "Jawad";
+console.log(myName);
+console.log(MyName);
+
+
+// constant in js 
+const a = 98;
+console.log(a);
+// a = 96; constant can't be assignable 
+// it also needs to be initialized while declaring 
