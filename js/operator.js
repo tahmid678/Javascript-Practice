0dc6fe52f9f1746cacdc4491e39ca731c8419a60
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2023-04-01 20:15:54 -0700

    operator in js

---
+++ js/operator.js
@@ -0,0 +1,17 @@
+//increment and decrement operator
+var a = 9;
+var b = 7;
+a++;
+++b;
+console.log(a);
+console.log(b);
+
+//post increment and pre increment
+var x = 4;
+var y = 5;
+var m = x++;
+var n = ++y;
+
+console.log(m);
+console.log(x);
+console.log(n);
