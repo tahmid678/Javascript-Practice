62196abed5b8ba9738c0d094800e4905112a1e5a
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2023-04-02 13:15:21 -0700

    template literal

---
+++ js/template literal.js
@@ -0,0 +1,25 @@
+//Template Literals
+console.log("First Line");
+console.log("Second Line");
+
+//Backtick
+console.log(`Third Line
+Fourth Line`);
+
+let a = `Fifth Line
+Sixth Line`;
+console.log(a);
+
+let age = 36;
+console.log(`His age is ${age}.`);
+
+let name = "Rahim";
+let dob = "21st June, 1983";
+console.log(`His name is ${ name }
+His age is ${age}
+His dob is ${dob}.`);
+
+let x = 34;
+let y = 56;
+
+console.log(`${x} + ${y} = ${x + y}`);
